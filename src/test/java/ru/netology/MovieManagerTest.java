87f2099d57b4bbccc1b956ed5c924619273bb700
Artiom Net <neartiom@yande.ru>
2024-10-10 21:31:02 +0400

    Patch 1

--- src/test/java/ru/netology/MovieManagerTest.java
+++ src/test/java/ru/netology/MovieManagerTest.java
@@ -62,4 +62,30 @@ class MovieManagerTest {
         String[] actual = manager.findLast();
         Assert.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void TestMinLimitMovie() {
+        MovieManager manager = new MovieManager(3);
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        String[] expected = {"Film 5", "Film 4", "Film 3"};
+        String[] actual = manager.findLast();
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void TestXLimitMovie() {
+        MovieManager manager = new MovieManager(5);
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        String[] expected = {"Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+        Assert.assertArrayEquals(expected, actual);
+    }
 }
